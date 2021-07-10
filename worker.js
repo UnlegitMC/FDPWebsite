b56d0f8e0769bf9911a3e0b0c13acc215991bdf8
liuli <liulihaocai@outlook.com>
2021-07-11 01:19:49 +0800

    update js

--- worker.js
+++ worker.js
@@ -1,14 +1,27 @@
+var json=
+{
+  version:"v1.2.3",
+  website: "fdp.liulihaocai.pw",
+  updatelog: [
+    {text:"FDPClient offical discord: https://discord.gg/dJtjF7swH9",color:{r:0,g:255,b:0,a:160}},
+      "",
+      "New things in FDPClient v1.2.3",
+      {text:"[+] INSANE RedeSky Velocity",color:{rainbow:"SKY",rainbow_index:0,alpha:160}}
+    ]
+};
+var jsonStr=JSON.stringify(json)
+
+
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-  })
-  
-  async function handleRequest(request) {
-    return new Response(JSON.stringify({version:"v114.514"})
-      , {
-          status: 200,
-          headers: {
-            "content-type": "text/json;charset=UTF-8",
-            "Access-Control-Allow-Origin": "*"
-          },
-        })
-  } 
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  return new Response(jsonStr, {
+        status: 200,
+        headers: {
+          "content-type": "text/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*"
+        },
+      })
+}
