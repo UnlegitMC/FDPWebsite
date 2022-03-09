a8c5ae143b15b5f5ec97a5761b52ae024605b7c4
liulihaocai <liulihaocaiqwq@gmail.com>
2022-03-09 21:16:06 +0800

    header and footer & better

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 import globals from '../components/globals'
+import Link from 'next/link'
 
 export default function Navbar() {
     return (
         <div>
-            <div className="navbar fixed bg-base-100">
+            <div className="navbar fixed bg-base-100 border-b">
                 <div className="navbar-start">
                     <a className="btn no-animation btn-ghost normal-case text-xl text-gradient">FDP Client</a>
                 </div>
                 <div className="navbar-center lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><a href='/'>Home</a></li>
-                        <li><a href='/download'>Download</a></li>
+                        <li><Link href='/'><a>Home</a></Link></li>
+                        <li><Link href='/download'><a>Download</a></Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
@@ -26,6 +28,21 @@ export default function Navbar() {
             <div className="navbar bg-base-100">
                 {/* placeholder */}
             </div>
+            <p className="navbar mx-auto menu-horizonal space-x-3 border-b">
+                &nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faCircleInfo} />
+                <span>UnlegitMC Team stands with Ukraine! 🇺🇦</span>
+                <button className='font-semibold absolute right-10 text-2xl' onClick={closeUkAlert}>
+                    <span>x</span>
+                </button>
+            </p>
         </div>
     )
+}
+
+function closeUkAlert(event) {
+    let element = event.target;
+    while (element.nodeName !== "BUTTON") {
+        element = element.parentNode;
+    }
+    element.parentNode.parentNode.removeChild(element.parentNode);
 }
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,11 @@ import Head from 'next/head'
 export default function MainPage() {
   return (
     <div>
-       <Head>
+      <Head>
         <title>Home - FDPClient</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <p className="font-mono font-medium text-xl">
-        Support Ukraine 🇺🇦 <a className="text-green-500 dark:text-green-300" href="https://opensource.fb.com/support-ukraine">Help Provide Humanitarian Aid to Ukraine.</a>
-      </p>
+      Hi, here is the content.
       <br />
       <br />
       <br />
